e98e3e5376223134b8b9143ed30fbeb3bc4812b4
Zam Titan <49342907+loopopski@users.noreply.github.com>
2023-02-21 11:18:30 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Basic annotations - add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    takes arguments and returns their
+     sum as a float.
+     :param a:
+     :param b:
+     :return :
+    """
+    return a + b
